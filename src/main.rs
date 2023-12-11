5da2755ad81a77cfa404b5fbaeac4745c4a3f09d
web2ls <web2ls@yandex.ru>
2023-12-11 23:04:21 +0400

    change err output

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments {err}");
+        eprintln!("Problem parsing arguments {err}");
         process::exit(1);
     });
 
     if let Err(e) =  minigrep::run(config) {
-        println!("error occure on run {e}");
+        eprintln!("error occure on run {e}");
         process::exit(1);
     }
 }
